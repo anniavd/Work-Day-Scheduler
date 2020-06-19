5965e8cb6e961047fb57926419fd543115ab70c1
annia valdes <millerl.mb79@gmail.com>
2020-06-19 00:25:16 -0500

    save a task in localStorage

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,38 @@
-///  localstorage
 
-var tasks= localStorage.getItem("tasks") 
-if(tasks){
+///  localstorage
+var tasks = [];
+
+var tasks = localStorage.getItem("tasks")
+if (tasks) {
     tasks = JSON.parse(tasks)
-} 
-else{
-    tasks={}
 }
+else {
+    tasks = [];
+}
+
+
+
+$('.saveBtn').on('click', function (event) {
+    event.preventDefault();
+
+    console.log($(this)[0].id)
+    var description = $(this).prev()[0].value.trim()
+
+    console.log(description)
+
+    tasks.push(description);
+    localStorage.setItem(($(this)[0].id), JSON.stringify(tasks))
+
+});
+
+
+
+//save task in localStorage
+
+/* var saveTasks = function() {
+   localStorage.setItem("tasks", JSON.stringify(tasks));
+};*/
+
 
 // showinfo()
 // showcolors() if hour < current addClass past
